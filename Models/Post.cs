7781e9efde21769cfc17055cc67ac2d0abac1fdb
Vhon Michael <vhontabalan@gmail.com>
2024-09-24 02:46:27 +0800

    Primary Models needeed, Need some more

---
+++ Models/Post.cs
@@ -0,0 +1,13 @@
+﻿namespace Galeriya.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string PostTitle { get; set; }
+        public string ImgUrl {  get; set; } //Image URL for storing Image
+        public DateTime UploadPost { get; set; }
+        public int UploadeyByUserId {  get; set; }
+        public User UploadedBy {  get; set; } //Navigation Property
+
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace Galeriya.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public String? ProfilePicture { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public DateTime JoinDate { get; set; }
+
+    }
+}
