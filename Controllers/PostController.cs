6842e56769e40cf1f5c8c1197ed179884b29d09e
Vhon Michael <vhontabalan@gmail.com>
2024-09-30 01:23:44 +0800

    Finished the PostController.cs, tho need some optimization, testing, and possible revision/refactoring.

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -1,6 +1,7 @@
 ﻿using Galeriya.Data; // To access the ApplicationDbContext 
 using Microsoft.AspNetCore.Mvc; //To inherit Controller Class
-using Galeriya.Models; // To use all the models
+using Galeriya.Models;
+using Microsoft.EntityFrameworkCore; // To use all the models
 
 namespace Galeriya.Controllers
 {
@@ -21,7 +22,7 @@ namespace Galeriya.Controllers
         //Get Posts
         public async Task<IActionResult> Index()
         {
-            return View(); //Probably need something here later on
+            return View(await _dbContext.Posts.ToListAsync()); //Probably need something here later on
         }
 
         public async Task<IActionResult> Details(int? id)
@@ -39,12 +40,93 @@ namespace Galeriya.Controllers
         }
 
         [HttpPost] //For Creating
-        public async Task<IActionResult> Create(Post post)
+        [ValidateAntiForgeryToken] // Security to prevent CSRF attacks
+        public async Task<IActionResult> Create( Post post)
         {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Add(post);
+                await _dbContext.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(post);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if(id == null)
+                return NotFound();
             
+            var post = await _dbContext.Posts.FindAsync(id);
+            return View(post);
+        }
+
+        [HttpPut]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("PostTitle, ImgUrl, UploadPost")] Post post)
+        {
+            if(id != post.Id)
+                return NotFound(); // If IDs do not match, return a 404 response
+
+            if (ModelState.IsValid)
+            {
+                _dbContext.Entry(post).State = EntityState.Modified;  //Mark the entity as modified
+                try
+                {
+                    _dbContext.Update(post);
+                    await _dbContext.SaveChangesAsync();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                        throw;
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(post);
+
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if(id == null)
+                return NotFound();
+            
+            var post = await _dbContext.Posts.FindAsync(id);
+
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return View(post);
+        }
+
+            
+        [HttpDelete, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeletePost(int? id)
+        {
+            if(id == null)
+                return NotFound();
+            
+            var post = await _dbContext.Posts.FindAsync(id);
+
+            if (post == null)
+            {
+                return NotFound();
+            }
+            
+            _dbContext.Posts.Remove(post);
+            await _dbContext.SaveChangesAsync(); // Save changes to the database
+
+            return RedirectToAction(nameof(Index));
         }
         
-        
+       
         
 
     }
