b923d25f2b25e579c9b044662d94ca50b88bd559
Vhon Michael <vhontabalan@gmail.com>
2024-10-04 23:47:50 +0800

    Started the Controller for the user model

---
+++ Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Galeriya.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Galeriya.Controllers;
+
+public class UserController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public UserController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        return View();
+    }
+    
+    
+}
